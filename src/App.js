a9a1b9c676217afec2b7c0d35b2e1a621adaa673
CristoforosChoras <christopher.choras@yahoo.com>
2022-06-24 15:18:24 +0300

    tittle css styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { useState} from "react"
 
 function App() {
-  
+    return(
+  <div className='App'>
+    <div className='TittleSection'>
+    <h1>Pokemon Stats</h1>
+    </div>
+  </div>)
 }
 
 export default App;
