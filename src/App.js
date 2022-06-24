e5821ed608c87079c98eb023d58278e4ed537e49
CristoforosChoras <christopher.choras@yahoo.com>
2022-06-24 16:44:10 +0300

    pokemon card

--- src/App.js
+++ src/App.js
@@ -4,19 +4,33 @@ import axios from "axios";
 
 function App() {
     const [pokemonName, setPokemonName] = useState("")
-
+    const [pokemon, setPokemon] = useState({
+        name: "",
+        species: "",
+        img: "", 
+        stats: "",
+        type: "",
+    })
     const searchPokemon = () => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
-            console.log(response);
+            setPokemon({ name: pokemonName,
+                 species: response.data.species.name,
+                  img: response.data.sprites.front_default, 
+                  stats: response.data.stats.map((stat) => stat.base_stat),
+                  type: response.data.types[0].type.name,
+        })
         })
     }
-    
+
     return (
         <div className='App'>
             <div className='TittleSection'>
                 <h1>Pokemon Stats</h1>
                 <input type="text" onChange={(event) => { setPokemonName(event.target.value) }}></input>
                 <button onClick={searchPokemon}>Search Pokemon</button>
+            </div>
+            <div className='pokemonCard'>
+                 
             </div>
         </div>)
 }
